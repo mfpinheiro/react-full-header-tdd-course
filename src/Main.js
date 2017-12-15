7cddd5ce8a0a86ea8863a1ea0c46f6033153a1b5
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-14 23:24:56 -0200

    :white_check_mark: Add first test to full header component.

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const FullHeader = ({ name }) => (
-    <h1>Hello {name}!</h1>
-);
-
-FullHeader.propTypes = {
-    name: PropTypes.string.isRequired,
-};
+const FullHeader = ({}) => <header />;
 
 export default FullHeader;
