88424cdd2208efbf57b11e17d486a290c8932978
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-14 23:20:29 -0200

    :hammer: fix stories for the full header component.

--- stories/Main.js
+++ stories/Main.js
@@ -3,50 +3,39 @@ import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
 storiesOf('React Full Header', module)
-    .add('with title', () => {
-        <FullHeader title="TDD" />;
-    })
-    .add('with title and subtitle', () => {
-        <FullHeader title="TDD" subtitle="JS TDD COURSE xD" />;
-    })
-    .add('with title, subtitle and bgColor', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="JS TDD COURSE xD"
-            bgColor="#329988"
-        />;
-    })
-    .add('with title, subtitle, bgColor and textColor', () => {
+    .add('with title', () => <FullHeader title="TDD" />)
+    .add('with title and subtitle', () => (
+        <FullHeader title="TDD" subtitle="JS TDD COURSE xD" />
+    ))
+    .add('with title, subtitle and bgColor', () => (
+        <FullHeader title="TDD" subtitle="JS TDD COURSE xD" bgColor="#329988" />
+    ))
+    .add('with title, subtitle, bgColor and textColor', () => (
         <FullHeader
             title="TDD"
             subtitle="JS TDD COURSE xD"
             bgColor="#329988"
             textColor="#FF9900"
-        />;
-    })
-    .add('with title, subtitle, bgColor, textColor and font', () => {
+        />
+    ))
+    .add('with title, subtitle, bgColor, textColor and font', () => (
         <FullHeader
             title="TDD"
             subtitle="JS TDD COURSE xD"
             bgColor="#329988"
             textColor="#FF9900"
             font="Cursive"
-        />;
-    })
-    .add('with title, subtitle, bgImage', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="JS TDD COURSE xD"
-            bgImg="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg"
-        />;
-    })
-    .add('with title, subtitle, video', () => {
+        />
+    ))
+    .add('with title, subtitle, bgImage', () => (
+        <FullHeader title="TDD" subtitle="JS TDD COURSE xD" bgImg="" />
+    ))
+    .add('with title, subtitle, video', () => (
         <FullHeader
             title="TDD"
             subtitle="JS TDD COURSE xD"
             textColor="#EBE9EB"
             textColor="#3299BB"
             video="http://thenewcode.com/assets/videos/polina.mp4"
-        />;
-    });
-
+        />
+    ));
