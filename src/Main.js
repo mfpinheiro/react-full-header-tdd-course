1d3c5f9f776ac7732e0b7f6dc0c19aee99622118
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-14 23:40:15 -0200

    :white_check_mark: Add tests to verify if a title element was passed and contains a specific value.

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FullHeader = ({}) => <header />;
+const FullHeader = ({ title }) => <header>{title && <h1>{title}</h1>}</header>;
+
+const propTypes = {
+    title: PropTypes.string,
+};
+
+FullHeader.propTypes = propTypes;
 
 export default FullHeader;
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -8,4 +8,16 @@ describe('<FullHeader />', () => {
         const wrapper = shallow(<FullHeader />);
         expect(wrapper.find('header')).to.have.length(1);
     });
+    it('should have h1 tag when title are passed', () => {
+        const wrapper = shallow(<FullHeader title={'TDD'} />);
+        expect(wrapper.find('h1')).to.have.length(1);
+    });
+    it('should not have h1 when title is not passed', () => {
+        const wrapper = shallow(<FullHeader />);
+        expect(wrapper.find('h1')).to.have.length(0);
+    });
+    it('should have h1 with the title passed', () => {
+        const wrapper = shallow(<FullHeader title="TDD" />);
+        expect(wrapper.find('h1').props().children).to.be.equal('TDD');
+    });
 });
